bde3a8ce7b4718ab5fdb505c085226627a2ab4b5
evertonhf <evertonhf@gmail.com>
2024-12-13 22:33:27 -0300

    Ajuste login

--- src/main/java/br/edu/imepac/clinica_medica/telas/ScreenBase.java
+++ src/main/java/br/edu/imepac/clinica_medica/telas/ScreenBase.java
@@ -2,15 +2,8 @@ package br.edu.imepac.clinica_medica.telas;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
 
 public abstract class ScreenBase extends JFrame {
-
-    private static final String URL = "jdbc:mysql://localhost:3306/clinica_medica";
-    private static final String USER = "root";
-    private static final String PASSWORD = "AzSx718293!";
-
-    protected static Connection connection;
 
     public void positionScreen(int x, int y, int width, int heigth) {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
